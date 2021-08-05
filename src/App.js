a3a08f67e7e64573754d6a3c6e324b34871f7ffa
Kritika Sharma <erkritika.sharmaa>
2021-08-05 18:16:39 +0530

    PracticeTest-1 newComp

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
 import React from "react";
 import "./App.css";
+import KeyPad from "./KeyPad";
 
 class App extends React.Component {
   state = {
-    FirstNum: 0,
-    SecondNum: 0,
+    FirstNum: "",
+    SecondNum: "",
     Operator: "",
-    FinalRes: 0,
-    isSecondNum: false,
+    FinalRes: "",
+    isOperatorClicked: false,
+  };
+  HandleAllOperationsAndCalculation = (input) => {
+    if (input === "+" || input === "-" || input === "%" || input === "X") {
+      if (input === "X") input = "*";
+      this.setState({
+        Operator: input,
+        FinalRes: this.state.FinalRes + "" + input,
+        isOperatorClicked: true,
+      });
+    } else if (input === "Result") {
+      let res = eval(
+        `${this.state.FirstNum}${this.state.Operator}${this.state.SecondNum}`
+      );
+      this.setState({ FinalRes: res });
+      this.setState({ FirstNum: res, Operator: "", SecondNum: "" });
+    } else {
+      if (this.state.isOperatorClicked) {
+        this.setState({
+          SecondNum: this.state.SecondNum + input,
+          FinalRes: this.state.FinalRes + "" + input,
+        });
+      } else if (!this.state.isOperatorClicked) {
+        this.setState({
+          FirstNum: this.state.FirstNum + input,
+          FinalRes: this.state.FinalRes + "" + input,
+        });
+      }
+    }
   };
   render = () => {
     return (
@@ -19,65 +48,7 @@ class App extends React.Component {
           </div>
 
           <div className="Number-Area">{this.state.FinalRes}</div>
-
-          <div
-            className="KeyPadArea"
-            onClick={(e) => {
-              // console.log(e.target.innerText);
-              let input = e.target.innerText;
-
-              if (
-                input === "+" ||
-                input === "-" ||
-                input === "%" ||
-                input === "X"
-              ) {
-                if (input === "X") input = "*";
-                this.setState({ Operator: input });
-              } else if (input === "Result") {
-                let res = eval(
-                  `${this.state.FirstNum}${this.state.Operator}${this.state.SecondNum}`
-                );
-                this.setState({ FinalRes: res });
-                this.setState({ FirstNum: res, Operator: "", SecondNum: 0 });
-              } else {
-                if (this.state.isSecondNum) {
-                  this.setState({ SecondNum: input });
-                } else {
-                  this.setState({ FirstNum: input, isSecondNum: true });
-                }
-              }
-            }}
-          >
-            <div className="operations">
-              <button>+</button>
-              <button className="sub">-</button>
-              <button>%</button>
-              <button>X</button>
-              <button className="result">Result</button>
-            </div>
-            <div className="keypad-Numbers">
-              <div className="Numbers-row">
-                <button>1</button>
-                <button>2</button>
-                <button>3</button>
-              </div>
-              <div className="Numbers-row">
-                <button>4</button>
-                <button>5</button>
-                <button>6</button>
-              </div>
-              <div className="Numbers-row">
-                <button>7</button>
-                <button>8</button>
-                <button>9</button>
-              </div>
-              <div className="Numbers-row">
-                {" "}
-                <button>0</button>
-              </div>
-            </div>
-          </div>
+          <KeyPad HandlerFunction={this.HandleAllOperationsAndCalculation} />
         </div>
       </>
     );
--- src/KeyPad.jsx
+++ src/KeyPad.jsx
@@ -1,3 +1,43 @@
 import React from "react";
-class KeyPad extends React.Component {}
+class KeyPad extends React.Component {
+  render = () => {
+    return (
+      <div
+        className="KeyPadArea"
+        onClick={(e) => {
+          this.props.HandlerFunction(e.target.innerText);
+        }}
+      >
+        <div className="operations">
+          <button>+</button>
+          <button className="sub">-</button>
+          <button>%</button>
+          <button>X</button>
+          <button className="result">Result</button>
+        </div>
+        <div className="keypad-Numbers">
+          <div className="Numbers-row">
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+          </div>
+          <div className="Numbers-row">
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+          </div>
+          <div className="Numbers-row">
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+          </div>
+          <div className="Numbers-row">
+            {" "}
+            <button>0</button>
+          </div>
+        </div>
+      </div>
+    );
+  };
+}
 export default KeyPad;
